b4a7256097bb31dca1a6bd8527574ccdeaa3189d
Knut Ahlers <knut@ahlers.me>
2018-02-05 07:17:16 +0100

    Add compare logic to prevent non required writes
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- generateZonefiles.py
+++ generateZonefiles.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import difflib
+import hashlib
 import os
+import os.path
+import sys
 
 # Third-party imports
 import dns.resolver
@@ -9,6 +13,7 @@ import dns.rdatatype
 import jinja2
 import yaml
 
+BLOCKSIZE = 65536
 DEFAULT_TTL = 3600
 
 
@@ -16,6 +21,33 @@ def default(d, key, default=None):
     if key in d:
         return d[key]
     return default
+
+
+def diff_files(file1, file2):
+    fromlines = []
+    tolines = []
+    if os.path.isfile(file1):
+        with open(file1) as ff:
+            fromlines = ff.readlines()
+    if os.path.isfile(file2):
+        with open(file2) as tf:
+            tolines = tf.readlines()
+
+    print(''.join(difflib.unified_diff(
+        fromlines, tolines, file1, file2)))
+
+
+def hash_file(filename):
+    if not os.path.isfile(filename):
+        return ""
+
+    hasher = hashlib.sha1()
+    with open(filename, 'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(BLOCKSIZE)
+    return hasher.hexdigest()
 
 
 def resolve_alias(entry):
@@ -30,6 +62,8 @@ def resolve_alias(entry):
     if len(answers) == 0:
         raise Exception(
             "Alias {} was not resolvable: No answers!".format(entry['alias']))
+
+    answers.sort()
 
     for rdata in answers:
         new_entry = entry.copy()
@@ -77,8 +111,10 @@ def write_zone(zone, ttl, soa, nameserver, mailserver, entries):
     with open("zones/tmp.{}".format(zone), 'w') as zf:
         zf.write(zone_content)
 
-    # FIXME (kahlers): Check if contents changed
-    os.rename("zones/tmp.{}".format(zone), "zones/db.{}".format(zone))
+    if hash_file("zones/tmp.{}".format(zone)) != hash_file("zones/db.{}".format(zone)):
+        print("Generated and replaced zone file for {}".format(zone))
+        diff_files("zones/db.{}".format(zone), "zones/tmp.{}".format(zone))
+        os.rename("zones/tmp.{}".format(zone), "zones/db.{}".format(zone))
 
 
 def main():
