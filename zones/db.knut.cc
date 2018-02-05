fb9e2fc3799137bd1ad5fc103b0f05712da79ef4
Knut Ahlers <knut@ahlers.me>
2018-02-05 07:02:18 +0100

    Remove all zone files
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- zones/db.knut.cc
+++
@@ -1,5 +0,0 @@
-knut.cc.  3600 IN SOA   ns1.kserver.biz. dns.ahlers.me. 1 7200 900 1209600 86400
-knut.cc.  3600 IN NS    ns1.kserver.biz.
-knut.cc.  3600 IN NS    ns2.kserver.biz.
-knut.cc.  3600 IN NS    ns3.kserver.biz.
-knut.cc.  3600 IN CNAME ddba39e146dcw.cloudfront.net.
--- zones/db.luzifer.cc
+++
@@ -1,7 +0,0 @@
-luzifer.cc.      3600 IN SOA   ns1.kserver.biz. dns.ahlers.me. 1 7200 900 1209600 86400
-luzifer.cc.      3600 IN NS    ns1.kserver.biz.
-luzifer.cc.      3600 IN NS    ns2.kserver.biz.
-luzifer.cc.      3600 IN NS    ns3.kserver.biz.
-2.luzifer.cc.    3600 IN CNAME kstuff.autodns.kserver.biz.
-cdn.luzifer.cc.  3600 IN CNAME cdn.luzifer.cc.s3.amazonaws.com.
-luzifer.cc.      3600 IN CNAME luzifer.cc.s3-website-us-east-1.amazonaws.com.
